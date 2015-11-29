fee71adbdaaf3fffd0f4df2f91565f260cfb920e
Ibanheiz <nicolas.ibanheiz@gmail.com>
2015-11-29 16:52:24 -0200

    Responsivando portóflio

--- front/js/modules/home/controllers/HomeController.js
+++ front/js/modules/home/controllers/HomeController.js
@@ -6,6 +6,7 @@ var controllersModule = require('./_index');
  * @ngInject
  */
 function HomeController($scope) {
+  $scope.navOffset = ($scope.isMobile) ? 50 : 60;
 }
 
 controllersModule.controller('HomeController', HomeController);
