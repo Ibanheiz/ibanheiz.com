2164e09cb25e68bde7c9c16f3e2247c49a7a42dc
Ibanheiz <nicolas.ibanheiz@gmail.com>
2016-02-24 01:48:32 -0300

    Adicionando imagens dos portfólios restantes

--- back/modules/home/portfolios.js
+++ back/modules/home/portfolios.js
@@ -1,20 +1,48 @@
 module.exports = [
   {
     id:           1,
-    titulo:       'Vai Ser Rimando',
-    descricao:    '#Rede social para cultura hip hop',
-    imagem:       'portfolio-vaiserrimando.jpg',
-    imagemAlt:    'Vai Ser Rimando'
+    titulo:       'Coletivo Educação',
+    descricao:    '#Plataforma de vendas de curso',
+    imagem:       'portfolio-coletivo.jpg',
+    imagemAlt:    'Coletivo Educação',
+    link:         '#',
+    finalizado:    false,
+    layoutMobile:  false,
+    itens: [
+      {
+        id:            1,
+        pagina:        'Home',
+        descricao:     'Descrição.',
+        imagem:        'coletivo/coletivo-home.jpg'
+      },
+      {
+        id:            2,
+        pagina:        'Núcleos',
+        descricao:     'Descrição.',
+        imagem:        'coletivo/coletivo-nucleos.jpg'
+      },
+      {
+        id:            3,
+        pagina:        'Categorias',
+        descricao:     'Descrição.',
+        imagem:        'coletivo/coletivo-categorias.jpg'
+      },
+      {
+        id:            4,
+        pagina:        'Curso',
+        descricao:     'Descrição.',
+        imagem:        'coletivo/coletivo-curso.jpg'
+      },
+      {
+        id:            5,
+        pagina:        'Login',
+        descricao:     'Descrição.',
+        imagem:        'coletivo/coletivo-login.jpg'
+      }
+    ]
   },
   {
     id:           2,
-    titulo:       'Loc Prime',
-    descricao:    '#Catálogo para produtos e serviços',
-    imagem:       'portfolio-locprime.jpg',
-    imagemAlt:    'Loc Prime'
-  },
-  {
-    id:           3,
     titulo:       'Notify-e',
     descricao:    '#Serviços de mensageria',
     imagem:       'portfolio-notifye.jpg',
@@ -27,21 +55,116 @@ module.exports = [
         id:            1,
         pagina:        'Serviço',
         descricao:     'Página apresenta os produtos e serviços de mensageria da notify-e, com uma breve descrição e link direto para a documentação de cada serviço.',
-        imagem:        'notifye-servicos.jpg'
+        imagem:        'notifye/notifye-servicos.jpg'
       },
       {
         id:            2,
         pagina:        'Planos',
         descricao:     'Exibição dos pĺanos de venda dos produtos e serviços. O usuário pode selecionar o plano desejado e a forma de pagamento.',
-        imagem:        'notifye-planos.jpg'
+        imagem:        'notifye/notifye-planos.jpg'
+      },
+      {
+        id:            3,
+        pagina:        'Contratação',
+        descricao:     'Descrição.',
+        imagem:        'notifye/notifye-contratacao.jpg'
+      },
+      {
+        id:            4,
+        pagina:        'Dashboard',
+        descricao:     'Descrição.',
+        imagem:        'notifye/notifye-dashboard.jpg'
+      },
+      {
+        id:            5,
+        pagina:        'Documentação',
+        descricao:     'Descrição.',
+        imagem:        'notifye/notifye-docs.jpg'
+      }
+    ]
+  },
+  {
+    id:           3,
+    titulo:       'Loc Prime',
+    descricao:    '#Catálogo para produtos e serviços',
+    imagem:       'portfolio-locprime.jpg',
+    imagemAlt:    'Loc Prime',
+    link:         'www.locprime.com.br',
+    finalizado:    true,
+    layoutMobile:  false,
+    itens: [
+      {
+        id:            1,
+        pagina:        'Home',
+        descricao:     'Descrição.',
+        imagem:        'locprime/locprime-home.jpg'
+      },
+      {
+        id:            2,
+        pagina:        'Produtos',
+        descricao:     'Descrição.',
+        imagem:        'locprime/locprime-produtos.jpg'
+      },
+      {
+        id:            3,
+        pagina:        'Serviços',
+        descricao:     'Descrição.',
+        imagem:        'locprime/locprime-servicos.jpg'
+      },
+      {
+        id:            4,
+        pagina:        'Eventos',
+        descricao:     'Descrição.',
+        imagem:        'locprime/locprime-eventos.jpg'
+      },
+      {
+        id:            5,
+        pagina:        'Relatórios',
+        descricao:     'Descrição.',
+        imagem:        'locprime/locprime-relatorios.jpg'
       }
     ]
   },
   {
     id:           4,
-    titulo:       'Coletivo Educação',
-    descricao:    '#Plataforma de vendas de curso',
-    imagem:       'portfolio-coletivo.jpg',
-    imagemAlt:    'Coletivo Educação'
+    titulo:       'Vai Ser Rimando',
+    descricao:    '#Rede social para cultura hip hop',
+    imagem:       'portfolio-vaiserrimando.jpg',
+    imagemAlt:    'Vai Ser Rimando',
+    link:         'vaiserrimando.com.br',
+    finalizado:    false,
+    layoutMobile:  false,
+    itens: [
+      {
+        id:            1,
+        pagina:        'Home',
+        descricao:     'Descrição.',
+        imagem:        'vsr/vsr-home.jpg'
+      },
+      {
+        id:            2,
+        pagina:        'Músicas',
+        descricao:     'Descrição.',
+        imagem:        'vsr/vsr-musicas.jpg'
+      },
+      {
+        id:            3,
+        pagina:        'Player',
+        descricao:     'Descrição.',
+        imagem:        'vsr/vsr-player.jpg'
+      },
+      {
+        id:            4,
+        pagina:        'Notícias',
+        descricao:     'Descrição.',
+        imagem:        'vsr/vsr-noticia.jpg'
+      },
+      {
+        id:            5,
+        pagina:        'Cadastro',
+        descricao:     'Descrição.',
+        imagem:        'vsr/vsr-cadastro.jpg'
+      }
+    ]
   }
 ];
